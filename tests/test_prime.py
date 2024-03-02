8d25a291b64a8339abc6fde2a7e7c22cd1461bd1
crypto-tesco <jesco_bee@protonmail.com>
2024-03-02 16:01:51 +0100

    added test

---
+++ tests/test_prime.py
@@ -0,0 +1,38 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def test_prime1():
+    number = 1
+    assert not is_prime(number)
+
+
+def test_prime2():
+    number = 2
+    assert is_prime(number)
+
+
+def test_prime3():
+    number = 3
+    assert is_prime(number)
+
+
+def test_prime4():
+    number = 4
+    assert not is_prime(number)
+
+
+def test_prime5():
+    number = 5
+    assert is_prime(number)
